6cfa0fbd07bc94488b97d72c780cadbe75fc754f
alvn <an@unsilo.com>
2015-01-22 13:25:21 +0100

    fix(version): upgraded to new version of jawn

--- src/main/java/app/config/RouteConfig.java
+++ src/main/java/app/config/RouteConfig.java
@@ -1,14 +1,15 @@
 package app.config;
 
-import app.controllers.MovieController;
 import net.javapla.jawn.AbstractRouteConfig;
 import net.javapla.jawn.AppContext;
+import app.controllers.MovieController;
+import app.controllers.SomeController;
 
 public class RouteConfig extends AbstractRouteConfig {
-
     @Override
     public void init(AppContext appContext) {
         route("/movie/id/{id}").action("single").to(MovieController.class);
+        route("/else").to(SomeController.class);
+        route("/language/{lang}/*long_id").get().action("lang").to(SomeController.class);
     }
-
 }
--- src/main/java/app/controllers/SomeController.java
+++ src/main/java/app/controllers/SomeController.java
@@ -4,4 +4,7 @@ import net.javapla.jawn.AppController;
 
 public class SomeController extends AppController {
 
+    public void getLang() {
+        respond().text("language is {0} - param id: {1}", language(), param("long_id"));
+    }
 }
