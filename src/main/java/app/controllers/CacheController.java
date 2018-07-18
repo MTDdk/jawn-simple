082882747e3dd249823f9a0be0e6ee5dc15b40b3
MTDdk <alvn@alvn.dk>
2018-07-18 16:57:19 +0200

    less seconds on cache example

--- src/main/java/app/controllers/CacheController.java
+++ src/main/java/app/controllers/CacheController.java
@@ -20,18 +20,20 @@ public class CacheController extends Controller {
         Random rnd = new Random();
         Integer id = rnd.nextInt(movies.size());
         String key = "movies"+id;
+        int numberOfSeconds = 15;
         
         Movie movie;
         if (cache.isSet(key)) {
             view("cache_message","Already cached");
             movie = cache.get(key);
         } else {
-            // caching for 30 seconds
+            // caching for numberOfSeconds seconds
             movie = movies.fetch(id);
-            cache.add(key, movie, 30);
+            cache.add(key, movie, numberOfSeconds);
             
             view("cache_message","Added the movie to cache");
         }
         view("movie",movie);
+        view("number_of_seconds",numberOfSeconds);
     }
 }
