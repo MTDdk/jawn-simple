696ea42e0fbc362a17f0e935c8488f6fa9ca64bf
MTDdk <mtd.legalus@gmail.com>
2019-02-18 20:32:40 +0100

    movies updatable

--- src/main/java/app/controllers/MovieController.java
+++ src/main/java/app/controllers/MovieController.java
@@ -12,6 +12,7 @@ import net.javapla.jawn.core.Results;
 import net.javapla.jawn.core.View;
 import net.javapla.jawn.core.mvc.GET;
 import net.javapla.jawn.core.mvc.POST;
+import net.javapla.jawn.core.mvc.PUT;
 import net.javapla.jawn.core.mvc.Path;
 
 @Path("/movie")
@@ -43,40 +44,25 @@ public class MovieController {
         
         return index();
     }
-
-/*    public void index() {
-        view("movies", movies.listMovies());
-        render("list");
-    }
-    
-    public void getSingle() {
-        Movie movie = movies.fetch(getId().asInt());
-        view("movies", Arrays.asList(movie));
-        render("list");
-    }
-    
-    public void postMovie() {
-        String title = param("title").asString();
-        Integer year = param("year").asInt(2000); // default value
-        
-        movies.add(new Movie(title, year));
-        redirect(); // redirect to index() of this Controller
-    }*/
     
     /**
      * Update movies
      */
-/*    public void putName() {
-        Movie movie = movies.fetch(param("pk").asInt());
-        movie.name = param("value").asString();
-        respond().status().ok();
+    
+    @PUT
+    @Path("/name")
+    public void putName(Context ctx) {
+        Movie movie = movies.fetch(ctx.param("pk").map(Integer::parseInt).get());
+        movie.name = ctx.param("value").get();
     }
-    public void putYear() {
-        Movie movie = movies.fetch(param("pk").asInt());
-        movie.year = param("value").asInt();
-        respond().status().ok();
+    
+    @PUT
+    @Path("/year")
+    public void putYear(Context ctx) {
+        Movie movie = movies.fetch(ctx.param("pk").map(Integer::parseInt).get());
+        movie.year = ctx.param("value").map(Integer::parseInt).get();
     }
-    */
+
     /* 
      * **********************************
      * Get the list in a different format
